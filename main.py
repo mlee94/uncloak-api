77fef1a3fd3aba54a92ab526013eb276832d52a2
Michael Lee <michaelleeacc@gmail.com>
2023-02-24 11:10:29 +1000

    Tidy up formatting

--- main.py
+++ main.py
@@ -40,16 +40,18 @@ app = FastAPI()
 
 # CORS Settings
 origins = [
-  "http://localhost",
-  "http://localhost:3000",
+    "http://localhost",
+    "http://localhost:3000",
+    "https://www.uncloak.ai",
 ]
 app.add_middleware(
-  CORSMiddleware,
-  allow_origins=origins,
-  allow_credentials=True,
-  allow_methods=["*"],
-  allow_headers=["*"],
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
+
 
 @app.post("/uploadfile/")
 async def create_upload_file(file: UploadFile):
